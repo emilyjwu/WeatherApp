eb62ec600ee2cc17418af59e2b132211a14af4e2
Wu, Emily Jaling <ewu64@gatech.edu>
2024-01-30 00:17:10 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Emily Wu on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
